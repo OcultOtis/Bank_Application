68eb8926e97eb6e623c22df9891b9a60a2168ac0
Jakub <jakub.gamrat1@op.pl>
2023-09-14 12:18:09 +0200

    graphic update

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText passEdit;
+    private CheckBox mCheckBox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        passEdit = findViewById(R.id.editTextNumberPassword);
+        mCheckBox = findViewById((R.id.checkBox2));
+
+        mCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+
+            @Override
+
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked){
+                if(isChecked){
+                    passEdit.setTransformationMethod(null);
+                }else{
+                    passEdit.setTransformationMethod(new PasswordTransformationMethod());
+                }
+            }
+        });
     }
 }
